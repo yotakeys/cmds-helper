cb3dd8655f7652dddbdaae510e788682d014a43b
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-02-24 18:56:52 +0700

    "feat: add simple logic"

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ cmd = sys.argv[1]
 if(cmd == "git"):
     out = sp.run(["git","add","."], shell=True)
     out = sp.run(["git","commit","-m",'"feat: add simple logic"'], shell=True)
+    out = sp.run(["git","push"], shell=True)
     
