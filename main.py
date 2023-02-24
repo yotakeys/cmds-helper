8237d597aa3d75e7b243b14049711131294b67ac
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-02-24 18:56:02 +0700

    "feat: add simple logic"

---
+++ main.py
@@ -0,0 +1,9 @@
+import subprocess as sp
+import sys
+
+cmd = sys.argv[1]
+
+if(cmd == "git"):
+    out = sp.run(["git","add","."], shell=True)
+    out = sp.run(["git","commit","-m",'"feat: add simple logic"'], shell=True)
+    
