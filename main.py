c7975789b4f507dfc15f735085c5713cf4bb22b5
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-03-08 18:32:48 +0700

    feat: add json read

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import subprocess as sp
 import sys
+import  json
 
-cmd = sys.argv[1]
-
-if(cmd == "git"):
-    out = sp.run(["git","add","."], shell=True)
-    out = sp.run(["git","commit","-m",'"feat: add simple logic"'], shell=True)
-    out = sp.run(["git","push"], shell=True)
+with open("test.json") as json_file:
+    command = sys.argv[1]
+    list_command =  json.load(json_file)
+    if command in list_command:
+        for cmd in list_command[command]:
+            cmd_split = cmd.split(",")
+            out = sp.run(cmd_split, shell=True)
+    else:
+        print("Command not found")
     
