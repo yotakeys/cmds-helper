675c9b5efb3dd6f635950af219569fd81ed61384
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-03-25 22:53:23 +0700

    feat: add params to pass

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ import subprocess as sp
 import sys
 import  json
 
+def changeParams(params, cmds):
+    
+    for index in range(len(params)):
+        id = "%{i}".format(i=index+1)
+        for cmd in range(len(cmds)):
+            cmds[cmd] = cmds[cmd].replace(id,params[index])
+        
+        return cmds
+        
+
 with open("test.json") as json_file:
     command = sys.argv[1]
     list_command =  json.load(json_file)
     if command in list_command:
-        for cmd in list_command[command]:
+        if len(sys.argv) > 2:
+            cmds = changeParams(sys.argv[2:], list_command[command])
+        
+        for cmd in cmds:
             cmd_split = cmd.split(",")
             out = sp.run(cmd_split, shell=True)
     else:
