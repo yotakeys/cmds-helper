3829a3a01f16504aa772a9a84351af936f042cdc
yoel-mc <yoel@mulaicoding.id>
2023-04-08 00:54:26 +0700

    feat: json helper command from comma delimitor to space

--- cmdshelper/helper.py
+++ cmdshelper/helper.py
@@ -1,5 +1,6 @@
 import os
 import json
+import re
 import subprocess as sp
 
 
@@ -11,14 +12,20 @@ class Helper:
         self.list_command = dict()
         self.readAllCmd(abs_path)
 
+    # def changeParams(self, params: list, cmds: list) -> list:
+    #     for index in range(len(params)):
+    #         id = "%{i}".format(i=index+1)
+    #         for cmd in range(len(cmds)):
+    #             cmds[cmd] = cmds[cmd].replace(id, "\"{params[index]}\"")
+
+    #         return cmds
+
     def changeParams(self, params: list, cmds: list) -> list:
+        for i in range(len(params)):
+            cmds = list(
+                map(lambda cmd: [params[i] if val == f'%{i+1}' else val for val in cmd], cmds))
 
-        for index in range(len(params)):
-            id = "%{i}".format(i=index+1)
-            for cmd in range(len(cmds)):
-                cmds[cmd] = cmds[cmd].replace(id, params[index])
-
-            return cmds
+        return cmds
 
     def readAllCmd(self, abs_path):
         for root, dirs, files in os.walk(abs_path):
@@ -28,14 +35,23 @@ class Helper:
                         new_read = json.load(json_file)
                         self.list_command.update(new_read)
 
+    def extract_command(string):
+        pattern = r'(?<!\\)(?:"[^"]*"|[^\s"]+)'
+        args = re.findall(pattern, string)
+        command = args.pop(0)
+        command = re.sub(r'\\(.)', r'\1', command)
+        allin = [command] + args
+        return allin
+
     def run(self, cmd: list):
-        command = cmd[0]
+        command = cmd.pop(0)
         if command in self.list_command:
-            if len(cmd) > 1:
-                cmds = self.changeParams(cmd[1:], self.list_command[command])
+            if len(cmd):
+                cmds_list = list(map(Helper.extract_command,
+                                 self.list_command[command]))
+                cmds = self.changeParams(cmd, cmds_list)
 
             for cmd in cmds:
-                cmd_split = cmd.split(",")
-                sp.run(cmd_split, shell=True)
+                sp.run(cmd, shell=True)
         else:
             print("Command not found")
